9f141ed7f149481a2b96878f25be58ed01d5b5bd
Rafael Grandsire <rafael.grandsire@gmail.com>
2017-01-30 22:51:40 -0200

    teste

--- programa/main.c
+++ programa/main.c
@@ -4,24 +4,28 @@
 #include "Mapa.h"
 
 int main(int argc, char const *argv[]) {
-    ///Entrada
-    int numLinhas, numColunas;
+	int numCores;
+	///Quantidade de argumentos certos e número no primeiro
+	int validArgument = (
+		(argc == 2) &&
+		(sscanf(argv[1], "%d", &numCores) == 1)
+	);	
+	int numLinhas, numColunas;	
+    ///Entrada padrão
     int validScan = scanf("%d%d", &numLinhas, &numColunas);
-    if (validScan == 2){
+	if (validArgument && validScan){
 		Mapa* mapa = criaMapa(numLinhas, numColunas);
-
-		int melhorSoma;
-		int numCores = atoi(argv[1]);
-
-		if (argc == 1) {
-		    melhorSoma = melhorSomaTotal(mapa, 1);
-		} else if (argc == 2) {
-		    melhorSoma = melhorSomaTotal(mapa, numCores);
+		for (int i = 0; i < numLinhas; i++) {
+		    for (int j = 0; j < numColunas; j++) {
+		        validScan = scanf("%d", getEnderecoPosicao(mapa, i, j));
+		        if (validScan != 1)
+		        	return 0;
+		    }
 		}
-
+		int melhorSoma = melhorSomaTotal(mapa, numCores);
 		printf("%d\n", melhorSoma);
-
 		///Liberação de memória
+		
 		liberaMapa(mapa);
 	}
     return 0;
